e9e6ae549cc6561ea6b267820e227c69accb38de
Guillaume W. Bres <guillaume.bressaix@gmail.com>
2016-08-20 18:39:26 +0200

    correct cross-compilation of opencv

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 // CXX
-#include <string>
 #include <sstream>
 #include <fstream>
 #include <iostream>
@@ -9,32 +8,29 @@ using namespace std;
 
 int scp_to_server( void );
 
-#ifdef USE_OPENCV
-	#include <opencv2/opencv.hpp>
-	#include <opencv2/imgproc/imgproc.hpp>
-	#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include "OpenCV/opencv_lib.h"
+using namespace cv;
 
-	using namespace cv;
-#endif
-
-// Shell 
 #define CAPTURE_OUTPUT "output.avi"
 #define SCP_SHELL_SCRIPT "./scp_to_server.sh"
 
 #include "Sensors/mcp3008/spi.h"
 #include "Sensors/mcp3008/mcp3008.h"
 #define MCP3008_SPI_CHIPSELECT 0
-#define MCP3008_CHANNEL0 0
-#define MCP3008_CHANNEL1 1
+#define MCP3008_LUM_SENSOR_CHX 0
+#define MCP3008_SHARP_SENSOR_CHX 1
 
-#define DARKNESS_THRESHOLD 25
+#define CAMERA_BRIGHTNESS_THRESHOLD 25
+#define CAMERA_BRIGHTNESS_HI_ALPHA 0.65
+#define CAMERA_BRIGHTNESS_HI_BETA 0.2
 
 int main( int argc, char **argv ){
 	
 	struct mcp3008_t mcp;
 	int value;
 
-	/*
 	Mat source;
 	VideoCapture video( 0 ); // default cam
 	
@@ -44,25 +40,29 @@ int main( int argc, char **argv ){
 
 	namedWindow( "main", CV_WINDOW_AUTOSIZE );
 	resizeWindow( "main", 50, 50 );
-	*/
-
-	mcp3008_init( &mcp, MCP3008_SPI_CHIPSELECT );
 	
-	value = mcp3008_capture( &mcp, MCP3008_CHANNEL0, SINGLE_ENDED_MODE );
-	std::cout<<"ADC value: "<<value<<"\n";
+	mcp3008_init( &mcp, MCP3008_SPI_CHIPSELECT );
 
-	/*
 	while(1){
+	
+		value = mcp3008_capture( &mcp,
+			MCP3008_LUM_SENSOR_CHX, SINGLE_ENDED_MODE );
+
+		if( value < CAMERA_BRIGHTNESS_THRESHOLD ){
+			modify_brightness( source, 
+				CAMERA_BRIGHTNESS_HI_ALPHA, CAMERA_BRIGHTNESS_HI_BETA );
+		}
 
 		video >> source;
+		
 		hog.detectMultiScale( source, found, 0, Size(8,8), Size(32,32), 1.05, 2 ); // detect HOG descriptor
 		std::cout<< "cv::HOG.detectMultiScale found " << found.size() << " object(s)\n";
 
 		for ( int k=0; k<found.size(); k++ )
 			rectangle( source, found[k].tl(), found[k].br(), Scalar(0,255,0), 2 ); // draw shape that was found
-	
-		imshow( "main", source );
-	} */
+		
+		imshow( "main", source ); 
+	} 
 
 	mcp3008_exit( &mcp );
 	return 0;
