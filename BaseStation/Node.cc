afb3e1c86a9e91cf59bd385ddd8a219b9c3c91a4
Guillaume W. Bres <guillaume.bressaix@gmail.com>
2018-04-26 21:30:35 +0200

    created Node object

---
+++ BaseStation/Node.cc
@@ -0,0 +1,324 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+#include <string>
+#include <iostream>
+using namespace std;
+
+#include "Node.h"
+
+Node::Node(void){
+	_setNetworkRole(NETWORK_END_DEVICE);
+}
+
+Node::Node(uint8_t role){
+	_setNetworkRole(role);
+}
+
+std::string Node::to_csv(int indent){
+	std::string result;
+	
+	for (int i=0; i<indent; i++)
+		result.append("\t");
+	result.append("<Node>\n");
+	
+	for (int i=0; i<indent; i++)
+		result.append("\t");
+	result.append("\t<ATMY>");
+	result.append(getATMY());
+	result.append("</ATMY>\n");
+
+	for (int i=0; i<indent; i++)
+		result.append("\t");
+	result.append("\t<Role>");
+	
+	if (isCoordinator())
+		result.append("Coordinator");
+	else if (isRouter())
+		result.append("Router");
+	else 
+		result.append("EndDevice");
+
+	result.append("</Role>\n");
+	
+	for (int i=0; i<indent; i++)
+		result.append("\t");
+	result.append("\t<STS>OK</STS>\n");
+
+	for (int i=0; i<indent; i++)
+		result.append("\t");
+	result.append("</Node>\n");
+
+	return result;
+}
+
+void Node::print(void){
+	cout << "ATBD: " << getATBD() << "\n";
+	cout << "PAN ID: " << getATID() << "\n";
+	cout << "ATMY: " << getATMY() << "\n";
+	cout << "ATDH: " << getATDH() << "\n";
+	cout << "ATDL: " << getATDL() << "\n";
+	cout << "ATCT: " << getATCT() << "\n";
+}
+
+uint8_t Node::isCoordinator(void){
+	if (_role & NETWORK_COORDINATOR)
+		return 0x01;
+	else
+		return 0x00;
+}
+
+uint8_t Node::isRouter(void){
+	if (_role & NETWORK_ROUTER)
+		return 0x01;
+	else
+		return 0x00;
+}
+
+uint8_t Node::isEndDevice(void){
+	if (_role & NETWORK_END_DEVICE)
+		return 0x01;
+	else
+		return 0x00;
+}
+
+uint8_t Node::getNetworkRole(void){
+	return _role;
+}
+
+void Node::_setNetworkRole(uint8_t role){
+	_role = role;
+}
+
+int Node::send_command(int fd, const char *msg){
+	int size = 0;
+	char c, buffer[3];
+	write(fd, msg, strlen(msg));
+
+	while (1){
+		read(fd, &c, 1);
+		buffer[size++] = c;
+		if (c == '\r')
+			break;
+	}
+
+	return 0;
+}
+
+int Node::send_command_readback(int fd, const char *msg, char *answer){
+	int size = 0;
+	char c, buffer[64];
+	write(fd, msg, strlen(msg));
+	usleep(100000);
+	while (1){
+		read(fd, &c, 1);
+		buffer[size++] = c;
+		if (c == '\r')
+			break;
+	}
+
+	strcpy(answer, buffer);
+	return size;
+}
+
+int Node::broadcast(const char *tty, const char *payload){
+	int fd;
+	int size;
+
+	fd = open(tty, O_RDWR);
+	if (fd < 0){
+		printf("Failed to open %s port\n", tty);
+		return -1;
+	}
+/*
+	if (send_command(fd, "+++", 3) < 0)
+		return -1;
+
+	if (send_command(fd, "ATDH0\r", 6) < 0)
+		return -1;
+	
+	if (send_command(fd, "ATDLFFFF\r", 9) < 0)
+		return -1;
+
+	if (send_command(fd, "ATCN\r", 5) < 0)
+		return -1;
+*/
+	size = write(fd, payload, strlen(payload));
+	close(fd);
+	return size; 
+}
+
+int Node::send_message(const char *tty, const char *dest, const char *payload){
+	int fd;
+	int size;
+	char addr[16];
+/*
+	fd = open(tty, O_RDWR);
+	if (fd < 0){
+		printf("Failed to open %s port\n", tty);
+		return -1;
+	}
+
+	if (send_command(fd, "+++", 3) < 0)
+		return -1;
+	
+	if (send_command(fd, "ATDH0\r", 6) < 0)
+		return -1;
+	
+	sprintf(addr, "ATDL%s\r", dest);
+	if (send_command(fd, addr, 9) < 0)
+		return -1;
+	
+	if (send_command(fd, "ATCN\r", 5) < 0)
+		return -1;
+*/
+	size = write(fd, payload, strlen(payload));
+	close(fd);
+	return size;
+}
+
+int Node::readSettings(const char *tty){
+	if (isCoordinator()){ // direct HW access
+		char answer[64];
+		int fd = open(tty, O_RDWR);
+
+		if (fd < 0){
+			printf("failed to open %s port\n", tty);
+			return -1;
+		}
+
+		send_command(fd, "+++"); usleep(500000); // enter CMD mode
+		send_command_readback(fd, "ATBD\r", answer);  // check baudrate
+		_setATBD(atoi(answer));
+		
+		send_command_readback(fd, "ATID\r", answer);  // PAN/Network ID 
+		_setATID(string(answer));
+		
+		send_command_readback(fd, "ATMY\r", answer);  // self ID 
+		_setATMY(string(answer));
+		
+		send_command_readback(fd, "ATDH\r", answer);  // dest ID 
+		_setATDH(string(answer));
+		
+		send_command_readback(fd, "ATDL\r", answer);  // dest ID 
+		_setATDL(string(answer));
+
+		send_command_readback(fd, "ATCT\r", answer);
+		_setATCT(atoi(answer));
+
+		send_command(fd, "ATCN\r"); // we're done
+		close(fd);
+		return 0;
+	}
+
+	return -1;
+}
+
+int Node::setSettings(const char *tty, 
+	const char *ID, const char *MY, 
+		const char *DH, const char *DL, 
+			int CT
+){
+	if (isCoordinator()){ // direct HW access
+		char cmd[64];
+		int fd = open(tty, O_RDWR);
+
+		if (fd < 0){
+			printf("failed to open %s port\n", tty);
+			return -1;
+		}
+
+		send_command(fd, "+++"); // enter CMD mode
+		// set CT value
+		sprintf(cmd, "ATCT%d\r", CT);
+		send_command(fd, cmd);
+		printf("%s\n", cmd);
+		_setATCT(CT);
+
+		// set PAN ID
+		sprintf(cmd, "ATID%s\r", ID);
+		send_command(fd, cmd); 
+		printf("%s\n", cmd);
+		_setATID(string(ID));
+
+		// set MY ID
+		sprintf(cmd, "ATMY%s\r", MY);
+		send_command(fd, cmd); 
+		printf("%s\n", cmd);
+		_setATMY(string(MY));
+
+		// set DH ADDR
+		sprintf(cmd, "ATDH%s\r", DH);
+		send_command(fd, cmd); 
+		printf("%s\n", cmd);
+		_setATDH(string(DH));
+
+		// set DL ADDR
+		sprintf(cmd, "ATDL%s\r", DL);
+		send_command(fd, cmd); 
+		printf("%s\n", cmd);
+		_setATDL(string(DL));
+
+		send_command(fd, "ATWR\r"); // confirm & store 
+		printf("ATWR\n");
+
+		send_command(fd, "ATCN\r"); // we're done
+		printf("ATCN\n");
+
+		close(fd);
+		return 0;
+	}
+		
+	return -1;
+}
+
+std::string Node::getATID(void){
+	return _ATID;
+}
+
+void Node::_setATID(string id){
+	_ATID = id;
+}
+
+std::string Node::getATMY(void){
+	return _ATMY;
+}
+
+void Node::_setATMY(string my){
+	_ATMY = my;
+}
+
+std:: string Node::getATDL(void){
+	return _ATDL;
+}
+
+void Node::_setATDL(string dl){
+	_ATDL = dl;
+}
+
+std::string Node::getATDH(void){
+	return _ATDH;
+}
+
+void Node::_setATDH(string dh){
+	_ATDH = dh;
+}
+
+int Node::getATBD(void){
+	return _ATBD;
+}
+
+void Node::_setATBD(int bd){
+	_ATBD = bd;
+}
+
+int Node::getATCT(void){
+	return _ATCT;
+}
+
+void Node::_setATCT(int ct){
+	_ATCT = ct;
+}
---
+++ BaseStation/Node.h
@@ -0,0 +1,110 @@
+#ifndef __NODE_H__
+#define __NODE_H__
+
+#define COMMAND_RETRY			128
+#define NETWORK_COORDINATOR 	0x01
+#define NETWORK_ROUTER 			(0x01<<1)
+#define NETWORK_END_DEVICE 	(0x01<<2)
+
+#include <string>
+
+class Node {
+	
+public:
+	// builds a Node with default 'End Device' network role 
+	Node(void);
+
+	// builds a Node with specified network role 
+	Node(uint8_t role);
+
+	int to_file(std::string fp);
+
+	// returns PAN/Network ID
+	std::string getATID(void);
+	
+	// returns self ID
+	std::string getATMY(void);
+	
+	// returns current Dest address (higher bits)
+	std::string getATDH(void);
+
+	// returns current Dest address (lower bits)
+	std::string getATDL(void);
+
+	// returns ATBD value (baud rate)
+	int getATBD(void);
+
+	// returns ATCT value (CMD dead time)
+	int getATCT(void);
+
+	// serializes self in .csv format
+	// indent: current indentation level
+	std::string to_csv(int indent);
+
+	// prints self in human readable format
+	void print(void);
+
+	// role within network 
+	uint8_t isCoordinator(void);
+	uint8_t isRouter(void);
+	uint8_t isEndDevice(void);
+	uint8_t getNetworkRole(void);
+
+	// broadcasts message over the network
+	// returns -1 in case of failure
+	// returns size_t on success
+	int broadcast(const char *tty, const char *payload);
+
+	// sends given message to DEST address
+	// returns -1 in case of failure
+	// returns size_t on success
+	int send_message(const char *tty, const char *dest, const char *payload);
+	
+	// sends message using opened serial port
+	// returns 0 if 'OK\r' received
+	// returns -1 otherwise
+	int send_command(int fd, const char *msg);
+
+	// sends message using opened serial port
+	// returns answer
+	int send_command_readback(int fd, const char *msg, char *answer);
+
+	// retrieves node settings
+	// if self.isCoordinator: HW access is directly provided
+	// returns -1 in case of failure
+	// failure: HW access was not granted
+	// failure: baud rate mismatch?
+	int readSettings(const char *tty);
+
+	// sets XBEE module up
+	// with specified PAN (Network) ID,
+	// MYID, Dest ADDR
+	// and CT time value
+	// returns -1 in case of failure
+	int setSettings(const char *tty, 
+		const char *ID, const char *MY, 
+			const char *DH, const char *DL, 
+				int CT
+	);
+
+private:
+	// role within network
+	uint8_t _role;
+	void _setNetworkRole(uint8_t role);
+	
+	std::string _ATID; // PAN / Network ID
+	std::string _ATMY; // self ADDR
+	std::string _ATDL, _ATDH; // dest ADDR
+	int _ATCT; // CMD dead time
+	int _ATBD; // Baud rate
+
+	void _setATID(std::string id);
+	void _setATMY(std::string my);
+	void _setATDL(std::string dl);
+	void _setATDH(std::string dh);
+	void _setATCT(int ct);
+	void _setATBD(int bd);
+	
+};
+
+#endif
