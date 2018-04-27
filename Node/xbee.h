de59ef56c5acb7702ffa91417eae62f462d247a9
Guillaume W. Bres <guillaume.bressaix@gmail.com>
2018-04-27 14:19:48 +0200

    XBEE API: added unicast, broadcast, improved send_command()

--- Node/xbee.h
+++ Node/xbee.h
@@ -1,12 +1,35 @@
 #ifndef __XBEE_H__
 #define __XBEE_H__
 
+#define XBEE_SLEEP BIT6
+
 // initializes UART for XBEE communcations
 // notifies BS we're live when done
 void xbee_init(void);
 
-// notifies base station with given status phrase
-// expects const char terminated by '\r\n'
-void xbee_send_base(const char *sts);
+// sends given command to XBEE module
+void xbee_send_command(const char *cmd);
+
+// sends given command to XBEE module
+// return 0 if expected confirmation phrase was answered
+// returns -1 otherwise
+int xbee_confirmed_command(const char *cmd, const char *confirm);
+
+// sends given command to XBEE module
+// returns number of bytes received & associated answer
+int xbee_answered_command(const char *cmd, char *answer);
+
+// sets XBEE module up using given parameters
+void xbee_setup(const char *ID, const char *MY, const char *DL);
+
+// puts XBEE module in hibernation mode (PIN mode)
+// or wakes it up
+void xbee_sleep(char sleep);
+
+// sends given message as broadcast
+void xbee_broadcast(const char *payload);
+
+// sends given message to specified dest ADDR
+void xbee_unicast(const char *payload, const char *DH, const char *DL);
 
 #endif
