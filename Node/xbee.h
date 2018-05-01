d482c507bc02d7350855979da1da6ad7cfba2221
Guillaume W. Bres <guillaume.bressaix@gmail.com>
2018-05-01 16:37:50 +0200

    working on BS/Node interactions

--- Node/xbee.h
+++ Node/xbee.h
@@ -1,6 +1,8 @@
 #ifndef __XBEE_H__
 #define __XBEE_H__
 
+#define BUFSIZE 64
+
 // initializes UART for XBEE communcations
 // notifies BS we're live when done
 void xbee_init(void);
@@ -9,14 +11,9 @@ void xbee_init(void);
 void xbee_send_command(const char *cmd, const int size);
 
 // sends given command to XBEE module
-// return 0 if expected confirmation phrase was answered
+// waits for answer terminated by '\r'
 // returns -1 otherwise
-int xbee_confirmed_command(const char *cmd, const int size, const char *confirm);
-
-// sends given command to XBEE module
-// returns number of bytes received
-// associated answer is located in UART rx buffer
-int xbee_answered_command(const char *cmd, const int size);
+int xbee_confirmed_command(const char *cmd, const int size); 
 
 // sets XBEE module up using given parameters
 void xbee_setup(const char *ID, const char *MY, const char *DL);
@@ -31,6 +28,9 @@ void xbee_broadcast(const char *payload, const int size);
 
 // sends given message to specified dest ADDR
 void xbee_unicast(const char *payload, const char *dl, const int size);
+
+// waits until a valid frame has been received
+void xbee_wait_instructions(char *buffer);
 
 // calculates checksum of given API frame
 uint8_t checksum(const char *API_frame);
