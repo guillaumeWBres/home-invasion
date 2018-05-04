fc843abb38d79a8ee27f735fb803429c1832cdf2
Guillaume W. Bres <guillaume.bressaix@gmail.com>
2018-05-04 23:37:15 +0200

    corrected database query

--- BaseStation/web-server/events.php
+++ BaseStation/web-server/events.php
@@ -1,9 +1,10 @@
 <?php 
 	
 	// retrieve calendar->jquery infos
-	$start = $_GET['start']; 
-	$end = $_GET['end'];
+	$start = $_POST['weekStart']; 
+	$end= $_POST['weekEnd']; 
 
+	// database description
 	$host = "localhost";
 	$username = "root";
 	$password = "root";
@@ -18,12 +19,10 @@
 	$connection->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 	$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-	// liste les evenments dans la .db
-	$query = "SELECT * FROM events where start > \"" .$start ."\" and end <= \"" .$end ."\""; 
-//	$query = "SELECT * FROM events where end <= \"2018-05-03\"";
-	echo $query;
+	// retrieve elements for this week
+	$query = "SELECT * FROM events where start >= \"" .$start . "\" and end <= \"" .$end ."\";";
 	$result = $connection->query($query) or die(print_r($connection->errorIno()));
-
+	
 	echo json_encode($result->fetchAll(PDO::FETCH_ASSOC));
 	exit;
 ?>
